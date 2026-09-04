9e21663b3979d3a28f1e6be33bbb689c11856710
Baptiste Henin <bhenin@ol.fr>
2026-09-04 08:30:24 +0200

    chore: initial commit - projet AutoML

---
+++ AutoML_2/Ensemble/README.md
@@ -0,0 +1,35 @@
+# Summary of Ensemble
+
+[<< Go back](../README.md)
+
+
+## Ensemble structure
+| Model             |   Weight |
+|:------------------|---------:|
+| 3_Default_Xgboost |        1 |
+
+### Metric details:
+| Metric   |       Score |
+|:---------|------------:|
+| MAE      | 0.00689605  |
+| MSE      | 0.000472143 |
+| RMSE     | 0.0217288   |
+| R2       | 0.996374    |
+| MAPE     | 3.55922e+12 |
+
+
+
+## Learning curves
+![Learning curves](learning_curves.png)
+## True vs Predicted
+
+![True vs Predicted](true_vs_predicted.png)
+
+
+## Predicted vs Residuals
+
+![Predicted vs Residuals](predicted_vs_residuals.png)
+
+
+
+[<< Go back](../README.md)
---
+++ automl_script.py
@@ -0,0 +1,36 @@
+# %% import librairies 
+import pandas as pd
+from supervised.automl import AutoML
+import IPython
+import markdown
+from loguru import logger 
+
+# %% load datasets and split train test 
+
+data_train = pd.read_csv("train_dataset.csv", encoding='UTF-8', sep=",")
+data_test = pd.read_csv("test_dataset.csv", encoding='UTF-8', sep=",")
+
+X_train = data_train.drop(columns=['TAUX_DE_REMPLISSAGE'])
+y_train = data_train['TAUX_DE_REMPLISSAGE']
+
+X_test = data_test.drop(columns=['TAUX_DE_REMPLISSAGE'])
+y_test = data_test['TAUX_DE_REMPLISSAGE']
+
+logger.info("X_train shape:")
+logger.info(X_train.shape)
+logger.info("y_train shape:")
+logger.info(y_train.shape)
+logger.info("X_test shape:")
+logger.info(X_test.shape)
+logger.info("y_test shape:")
+logger.info(y_test.shape)
+
+# %% fit model, predict and then generate html report 
+automl = AutoML(total_time_limit=5*60, mode='Explain', random_state=42, ml_task='regression')
+automl.fit(X_train, y_train)
+predictions = automl.predict(X_test)
+report = automl.report()  # also writes AutoML_1/README.html on disk
+
+# %% save the html report (must stay in the results folder: the images are relative links)
+with open("AutoML_1/rapport.html", "w", encoding='UTF-8') as f:
+    f.write(report.data)
