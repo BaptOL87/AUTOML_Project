d404d02c4dcd8010200ca05ab4d4ca10f6414cd5
Baptiste Henin <bhenin@ol.fr>
2026-09-25 15:19:33 +0200

    refactor : modif de la structure et mise des runs précédents dans AutoML_previous_runs

--- automl_script.py
+++ automl_script.py
@@ -11,10 +11,10 @@ data_train = pd.read_csv("train_dataset.csv", encoding='UTF-8', sep=",")
 data_test = pd.read_csv("test_dataset.csv", encoding='UTF-8', sep=",")
 
 X_train = data_train.drop(columns=['TAUX_DE_REMPLISSAGE', 'TAUX_DE_VENTE', 'UPDATE_TIMESTAMP_UTC'])
-y_train = data_train['TAUX_DE_VENTE']
+y_train = data_train['TAUX_DE_REMPLISSAGE']
 
 X_test = data_test.drop(columns=['TAUX_DE_REMPLISSAGE', 'TAUX_DE_VENTE', 'UPDATE_TIMESTAMP_UTC'])
-y_test = data_test['TAUX_DE_VENTE']
+y_test = data_test['TAUX_DE_REMPLISSAGE']
 
 logger.info("X_train shape:")
 logger.info(X_train.shape)
@@ -26,8 +26,22 @@ logger.info("y_test shape:")
 logger.info(y_test.shape)
 
 # %% fit model, predict and then generate html report 
-automl = AutoML(total_time_limit=5*60, mode='Explain', random_state=42, ml_task='regression')
-automl.fit(X_train, y_train)
+# automl = AutoML(
+#     total_time_limit=5*60,
+#     mode='Explain',
+#     random_state=42,
+#     ml_task='regression'
+# )
+
+automl = AutoML(   
+            mode="Perform",    
+            validation_strategy={"validation_type": "custom"},   
+            eval_metric="mae",    
+            results_path="automl_chrono"
+            )
+
+
+automl.fit(X_train, y_train, cv=[(X_train, y_train)])
 predictions = automl.predict(X_test)
 report = automl.report()  # also writes AutoML_1/README.html on disk
 
